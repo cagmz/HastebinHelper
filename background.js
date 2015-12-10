8482f38e6bfa87a390d575ae43cec9e7b5fc86bf
Carlos Adrian Gomez <cagmz@users.noreply.github.com>
2015-12-10 23:37:37 +0000

    Added main extension files

---
+++ background.js
@@ -0,0 +1,29 @@
+// Set up menu and add listener to it
+chrome.runtime.onInstalled.addListener(init());
+// Add click event
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Create menu item
+function init() {
+	var title = "Hastebin Helper";
+	var hasteUrlMenu = chrome.contextMenus.create({"id": "child1", "title": "New Haste", "contexts":["selection"]});
+}
+
+// The onClicked callback function
+ function onClickHandler(info, tab) {
+ 	var post = new XMLHttpRequest();
+ 	var postData = JSON.stringify(info.selectionText);
+ 	var hastebinUrl = "http://hastebin.com/"
+	post.open("POST", "http://hastebin.com/documents", true);
+	post.setRequestHeader("Content-type", "application/json");
+	// If a successful connection is made, grab the new Haste key from the server's response
+	post.onreadystatechange = function() {
+		if(post.readyState == 4 && post.status == 200) {
+			var parsedData = JSON.parse(post.responseText);
+			var hasteKey = parsedData.key;
+			var hasteUrl = hastebinUrl + hasteKey;
+			chrome.tabs.create({ url: hasteUrl });
+		}
+	}
+	post.send(postData);
+};
